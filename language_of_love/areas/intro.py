8c20ecbd2f993c7cd6c6a2041731c5f82f97a248
Tristan Kells <tristankells@gmail.com>
2019-08-01 03:56:49 +1200

    Refactored menu and practice class

--- language_of_love/areas/intro.py
+++ language_of_love/areas/intro.py
@@ -12,16 +12,8 @@ def Introduction(intent_name, session_variables):
         """
         speech_text = Translator.Tutorial.answer_to_your_name
 
-        return Response(speech_text, session_variables=session_variables)
-
-    def where_are_you_from(session_variables):
-        """
-        Handler when the player ask's their date where they are from
-        """
-        # TODO: No longer needed after the tutorial / intro.py rework
+        # Break the intro loop and enter menu loop
         session_variables.area = AreaEnum.menu
-        speech_text = Translator.Tutorial.answer_to_question_where_are_you_from
-
         return Response(speech_text, session_variables=session_variables)
 
     def fallback(session_variables):
@@ -34,8 +26,7 @@ def Introduction(intent_name, session_variables):
         return Response(speech_text, session_variables=session_variables)
 
     intent_dictionary = {
-        Intents.ANSWER_NAME: my_name_is,
-        Intents.QUESTION_WHERE_YOU_FROM: where_are_you_from
+        Intents.ANSWER_NAME: my_name_is
     }
 
     return response_handler(intent_name, intent_dictionary, session_variables, fallback)
