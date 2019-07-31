34a19676fe5648288c0ea35ffa43fae394d3db32
EssaThaver <50125832+EssaThaver@users.noreply.github.com>
2019-08-01 04:04:57 +1200

    Added a few comments

--- language_of_love/audio.py
+++ language_of_love/audio.py
@@ -1,5 +1,7 @@
 class Audio():
+    #Start audio file for practice
     practice_start = "<audio src='https://s3.amazonaws.com/language-of-love/lecturer-practice-start.mp3'/>"
+    #Questions in english represendted by the e. For practice
     Q_e_lecturer_like_chocolate = "<audio src='https://s3.amazonaws.com/language-of-love/english-lecturer-do-u-like-chocolate.mp3'/>"
     Q_e_lecturer_like_coffee = "<audio src='https://s3.amazonaws.com/language-of-love/english-lecturer-do-u-like-coffee.mp3'/>"
     Q_e_lecturer_like_flower = "<audio src='https://s3.amazonaws.com/language-of-love/english-lecturer-do-u-like-flowers.mp3'/>"
@@ -7,7 +9,7 @@ class Audio():
     Q_e_lecturer_like_travel = "<audio src='https://s3.amazonaws.com/language-of-love/english-lecturer-do-u-like-travel.mp3'/>"
     Q_e_lecturer_what_name = "<audio src='https://s3.amazonaws.com/language-of-love/english-lecturer-whats-ur-name.mp3'/>"
     Q_e_lecturer_what_profession = "<audio src='https://s3.amazonaws.com/language-of-love/english-lecturer-whats-ur-profession.mp3'/>"
-
+    #Questions in spanish represendted by the s. For practice
     Q_s_lecturer_like_chocolate = "<audio src='https://s3.amazonaws.com/language-of-love/spanish-lecturer-do-u-like-chocolate.mp3'/>"
     Q_s_lecturer_like_coffee = "<audio src='https://s3.amazonaws.com/language-of-love/spanish-lecturer-do-u-like-coffee.mp3'/>"
     Q_s_lecturer_like_flower = "<audio src='https://s3.amazonaws.com/language-of-love/spanish-lecturer-do-u-like-flowers.mp3'/>"
@@ -15,9 +17,10 @@ class Audio():
     Q_s_lecturer_like_travel = "<audio src='https://s3.amazonaws.com/language-of-love/spanish-lecturer-do-u-like-travel.mp3'/>"
     Q_s_lecturer_what_name = "<audio src='https://s3.amazonaws.com/language-of-love/spanish-lecturer-whats-ur-name.mp3'/>"
     Q_s_lecturer_what_profession = "<audio src='https://s3.amazonaws.com/language-of-love/spanish-lecturer-whats-ur-profession.mp3'/>"
-    # hgjaghj
+    #In-game audio
     point = "<audio src='https://s3.amazonaws.com/language-of-love/point.mp3'/>"
     cricket_sound = "<audio src='https://s3.amazonaws.com/language-of-love/cricket_sound.mp3'/>"
+    #Intro files
     welcome = "<audio src ='https://s3.amazonaws.com/language-of-love/tristan-welcome.mp3'/>"
     welcome_2 = "<audio src = 'https://s3.amazonaws.com/language-of-love/tristan-welcome-2.mp3'/>"
     fantastico = "<audio src='https://s3.amazonaws.com/language-of-love/tristan-fantastico.mp3'/>"
