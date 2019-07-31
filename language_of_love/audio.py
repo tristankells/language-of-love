2457daf0b62575b4ae51c726be6193262307054e
EssaThaver <50125832+EssaThaver@users.noreply.github.com>
2019-08-01 10:32:55 +1200

    Added tessa audio

--- language_of_love/audio.py
+++ language_of_love/audio.py
@@ -1,7 +1,7 @@
 class Audio():
-    #Start audio file for practice
+    # Start audio file for practice
     practice_start = "<audio src='https://s3.amazonaws.com/language-of-love/lecturer-practice-start.mp3'/>"
-    #Questions in english represendted by the e. For practice
+    # Questions in english represendted by the e. For practice
     Q_e_lecturer_like_chocolate = "<audio src='https://s3.amazonaws.com/language-of-love/english-lecturer-do-u-like-chocolate.mp3'/>"
     Q_e_lecturer_like_coffee = "<audio src='https://s3.amazonaws.com/language-of-love/english-lecturer-do-u-like-coffee.mp3'/>"
     Q_e_lecturer_like_flower = "<audio src='https://s3.amazonaws.com/language-of-love/english-lecturer-do-u-like-flowers.mp3'/>"
@@ -9,7 +9,7 @@ class Audio():
     Q_e_lecturer_like_travel = "<audio src='https://s3.amazonaws.com/language-of-love/english-lecturer-do-u-like-travel.mp3'/>"
     Q_e_lecturer_what_name = "<audio src='https://s3.amazonaws.com/language-of-love/english-lecturer-whats-ur-name.mp3'/>"
     Q_e_lecturer_what_profession = "<audio src='https://s3.amazonaws.com/language-of-love/english-lecturer-whats-ur-profession.mp3'/>"
-    #Questions in spanish represendted by the s. For practice
+    # Questions in spanish represendted by the s. For practice
     Q_s_lecturer_like_chocolate = "<audio src='https://s3.amazonaws.com/language-of-love/spanish-lecturer-do-u-like-chocolate.mp3'/>"
     Q_s_lecturer_like_coffee = "<audio src='https://s3.amazonaws.com/language-of-love/spanish-lecturer-do-u-like-coffee.mp3'/>"
     Q_s_lecturer_like_flower = "<audio src='https://s3.amazonaws.com/language-of-love/spanish-lecturer-do-u-like-flowers.mp3'/>"
@@ -17,12 +17,29 @@ class Audio():
     Q_s_lecturer_like_travel = "<audio src='https://s3.amazonaws.com/language-of-love/spanish-lecturer-do-u-like-travel.mp3'/>"
     Q_s_lecturer_what_name = "<audio src='https://s3.amazonaws.com/language-of-love/spanish-lecturer-whats-ur-name.mp3'/>"
     Q_s_lecturer_what_profession = "<audio src='https://s3.amazonaws.com/language-of-love/spanish-lecturer-whats-ur-profession.mp3'/>"
-    #In-game audio
+    # In-game audio
     point = "<audio src='https://s3.amazonaws.com/language-of-love/point.mp3'/>"
     cricket_sound = "<audio src='https://s3.amazonaws.com/language-of-love/cricket_sound.mp3'/>"
-    #Intro files
+    # Intro files
     welcome = "<audio src ='https://s3.amazonaws.com/language-of-love/tristan-welcome.mp3'/>"
     welcome_2 = "<audio src = 'https://s3.amazonaws.com/language-of-love/tristan-welcome-2.mp3'/>"
     fantastico = "<audio src='https://s3.amazonaws.com/language-of-love/tristan-fantastico.mp3'/>"
     mucho_gusto = "<audio src='https://s3.amazonaws.com/language-of-love/tristan-mucho_gusto.mp3'/>"
     pracrice_or_date = "<audio src='https://s3.amazonaws.com/language-of-love/practice_or_speed_date.mp3'/>"
+    # Tessa audio files
+    Q_tessa_how_are_you = "<audio src='https://s3.amazonaws.com/language-of-love/tessa_how_are_you.mp3'/>"
+    Q_tessa_how_old_are_you = "<audio src='https://s3.amazonaws.com/language-of-love/tessa_how_old_are_you.mp3'/>"
+    Q_tessa_how_was_your_day = "<audio src='https://s3.amazonaws.com/language-of-love/tessa_how_was_your_day.mp3'/>"
+    Q_tessa_i_am_from_Wellington = "<audio src='https://s3.amazonaws.com/language-of-love/tessa_i_am_from_Wellington.mp3'/>"
+    Q_tessa_What_is_your_job = "<audio src='https://s3.amazonaws.com/language-of-love/tessa_What_is_your_job.mp3'/>"
+    Q_tessa_what_time_is_it = "<audio src='https://s3.amazonaws.com/language-of-love/tessa_what_time_is_it.mp3'/>"
+    Q_tessa_whats_your_favourite_colour = "<audio src='https://s3.amazonaws.com/language-of-love/tessa_whats_your_favourite_colour.mp3'/>"
+    Q_tessa_whats_your_name = "<audio src='https://s3.amazonaws.com/language-of-love/tessa_whats_your_name.mp3'/>"
+    Q_tessa_where_are_you_from = "<audio src='https://s3.amazonaws.com/language-of-love/tessa_where_are_you_from.mp3'/>"
+    Q_tessa_I_am_lawyer = "<audio src='https://s3.amazonaws.com/language-of-love/tessa_I_am_lawyer.mp3'/>"
+    Q_tessa_i_am_very_tired = "<audio src='https://s3.amazonaws.com/language-of-love/tessa_i_am_very_tired.mp3'/>"
+    A_tessa_im_25_years_old = "<audio src='https://s3.amazonaws.com/language-of-love/tessa_im_25_years_old.mp3'/>"
+    A_tessa_it_is_2_pmn = "<audio src='https://s3.amazonaws.com/language-of-love/tessa_i_am_from_Wtessa_it_is_2_pmellington.mp3'/>"
+    A_tessa_my_favourite_colour_is_blue = "<audio src='https://s3.amazonaws.com/language-of-love/tessa_my_favourite_colour_is_blue.mp3'/>"
+    A_tessa_My_name_is_Tess = "<audio src='https://s3.amazonaws.com/language-of-love/tessa_My_name_is_Tess.mp3'/>"
+    A_tessa_very_good_thanks = "<audio src='https://s3.amazonaws.com/language-of-love/tessa_very_good_thanks.mp3'/>"
