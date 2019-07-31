80fe664de45d8093f0a157c85aa266db7829d2d8
EssaThaver <50125832+EssaThaver@users.noreply.github.com>
2019-07-31 20:43:20 +1200

    Added more audio files

--- language_of_love/audio.py
+++ language_of_love/audio.py
@@ -1,3 +1,6 @@
 class Audio():
-    tutorial = "<audio src='https://s3.amazonaws.com/language-of-love/Tutorial.mp3'/>"
+    tutorial = "<audio src='https://s3.amazonaws.com/language-of-love/tutorial.mp3'/>"
     cricket_sound = "<audio src='https://s3.amazonaws.com/language-of-love/cricket_sound.mp3'/>"
+    welcome = "<audio src='https://s3.amazonaws.com/language-of-love/welcome.mp3'/>"
+    fantastico = "<audio src='https://s3.amazonaws.com/language-of-love/fantastico.mp3'/>"
+    mucho_gusto = "<audio src='https://s3.amazonaws.com/language-of-love/mucho_gusto.mp3'/>"
