c8b6b6e0d596715b2a0c9f1bea6e35aeddef8896
spare <aidan.masonmackay@outlook.com>
2019-08-01 03:33:14 +1200

    Added luanch audio

--- language_of_love/lambda_function.py
+++ language_of_love/lambda_function.py
@@ -164,41 +164,6 @@ def can_handle(handler_input):
     y = int(session_attr[PLACE])
     handler_input.attributes_manager.session_attributes = session_attr
     return is_intent_name(IntentList[z][y])(handler_input)
-
-def handle(self, handler_input):
-    # type: (HandlerInput) -> Response
-    handler_input.attributes_manager.session_attributes = session_attr
-    z = int(session_attr[CONVERSATION])
-    y = int(session_attr[PLACE])
-    speech_text = ResponseDict[IntentList[z][y]]
-
-    if y == 1:
-        session_attr[PLACE] = 0
-        session_attr[CONVERSATION] = 'None'
-    handler_input.attributes_manager.session_attributes = session_attr
-    handler_input.response_builder.speak(speech_text).set_card(
-        SimpleCard("Hello World", speech_text)).set_should_end_session(
-        False)
-    return handler_input.response_builder.response
-
-
-class CatchAllExceptionHandler(AbstractExceptionHandler):
-    """Catch all exception handler, log exception and
-    respond with custom message.
-    """
-
-    def can_handle(self, handler_input, exception):
-        # type: (HandlerInput, Exception) -> bool
-        return True
-
-    def handle(self, handler_input, exception):
-        # type: (HandlerInput, Exception) -> Response
-        logger.error(exception, exc_info=True)
-
-        speech = "Sorry, there was some problem. Please try again!!"
-        handler_input.response_builder.speak(speech).ask(speech)
-
-        return handler_input.response_builder.response
 # endregion
 
 # # # Date request_handlers # # # 
