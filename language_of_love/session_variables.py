ba6430f5f8d761aaa820d26a750b08e8a269a1a3
spare <aidan.masonmackay@outlook.com>
2019-08-01 13:31:29 +1200

    Change place variable

--- language_of_love/session_variables.py
+++ language_of_love/session_variables.py
@@ -36,8 +36,8 @@ class SessionVariables:
         self.tutorial_repeat_or_new = state_variables[
             self.PRACTICE_REPEAT_OR_NEW] if self.PRACTICE_REPEAT_OR_NEW in state_variables else False
 
-        self.conversation = state_variables[self.CONVERSATION]
-        self.place = state_variables[self.PLACE] if self.CONVERSATION in state_variables else 0
+        self.conversation = state_variables[self.CONVERSATION] if self.CONVERSATION in state_variables else 'None'
+        self.place = state_variables[self.PLACE] if self.PLACE in state_variables else 0
 
     @staticmethod
     def get_initial():
