40f9f55a598975f9a97da5ba6b351d4a8763b857
spare <aidan.masonmackay@outlook.com>
2019-08-01 13:34:57 +1200

    Add in Tessa's audio

--- language_of_love/date_intents.py
+++ language_of_love/date_intents.py
@@ -1,12 +1,12 @@
 import json
 from audio import Audio
 
-IntentList = [["QuestionWhereYouFromIntent", Audio.Q_tessa_i_am_from_Wellington],
+IntentList = [["QuestionWhereYouFromIntent", "AnswerWhereYouFromIntent"],
               ["QuestionJobIntent", "AnswerMyJobIntent"],
               ["QuestionAnimalsIntent", "AnswerAnimalIntent"]
               ]
 
-ResponseDict = {"QuestionWhereYouFromIntent": "I'm from Columbia",
+ResponseDict = {"QuestionWhereYouFromIntent": Audio.Q_tessa_i_am_from_Wellington,
                 "AnswerWhereYouFromIntent": "I love that place",
                 "QuestionJobIntent": "I am a doctor, what do you do?",
                 "AnswerMyJobIntent": "Great",
