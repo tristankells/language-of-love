c0dc7fab396bfcf7b8acd8856d27fcbda75c572d
spare <aidan.masonmackay@outlook.com>
2019-08-01 13:10:52 +1200

    Temporarily changing default area to date

--- language_of_love/session_variables.py
+++ language_of_love/session_variables.py
@@ -43,7 +43,7 @@ class SessionVariables:
         session_variables = {
             SessionVariables.FIRST_TIME: True,
             SessionVariables.NAME: "default",
-            SessionVariables.AREA: AreaEnum.introduction,
+            SessionVariables.AREA: AreaEnum.speed_date,
             SessionVariables.GENDER_PREFERENCE: GenderPreferenceEnum.both,
             SessionVariables.CURRENT_PRACTICE_PHRASE: PracticePhrases.WHAT_IS_YOUR_NAME,
             SessionVariables.PRACTICE_REPEAT_OR_NEW: False,
