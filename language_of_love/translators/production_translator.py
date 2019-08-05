6095f857ccc15117ecdfaff89322cfe4d3c34318
Tristan Kells <tristankells@gmail.com>
2019-08-06 00:12:59 +1200

    Trying to get the different voice in the game

--- language_of_love/translators/production_translator.py
+++ language_of_love/translators/production_translator.py
@@ -39,7 +39,7 @@ class Translator():
         fallback = "Sorry didn't understand that " + new_or_repeat
 
     class SpeedDate:
-        begin = '<voice name="Ivy">Dating has not been implemented, come back soon to practice your spanish skills and see if you can find love</voice>'
+        begin = '<voice name="Nicole">Dating has not been implemented, come back soon to practice your spanish skills and see if you can find love</voice>'
 
     class Error:
         bad_option = "Not an option right now."
