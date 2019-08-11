909568e085a11b4f0a13ac504dd27abd45b674ac
Tristan Kells <tristankells@gmail.com>
2019-08-11 21:43:48 +1200

    Added some speed date round checking

--- language_of_love/lambda_function.py
+++ language_of_love/lambda_function.py
@@ -153,7 +153,7 @@ def handle_date(handler_input):
 
     # If date over, add finishing date dialog
     if (session_attr.date_round >= 4):
-        speech_text = + "You finsished the date, your score is " + session_attr.date_score + ""
+        speech_text += "You finsished the date, your score is " + session_attr.date_score + ""
     handler_input.attributes_manager.session_attributes = session_attr.get_dict()
     handler_input.response_builder.speak(speech_text).set_card(
         SimpleCard("Hello World", speech_text)).set_should_end_session(
