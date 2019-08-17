099e5ce495b05205193a10ff962680c13fff1a3d
Tristan Kells <tristankells@gmail.com>
2019-08-17 22:29:00 +1200

    Added audio helper function to reduce line length in Audio class

--- language_of_love/custom_collections/audio.py
+++ language_of_love/custom_collections/audio.py
@@ -1,63 +1,69 @@
-class Audio():
+class Audio:
+    ROOT_URL = 'https://s3.amazonaws.com/language-of-love/'
+
+    @staticmethod
+    def get_audio(audio_name):
+        return "<audio src='" + Audio.ROOT_URL + audio_name + ".mp3'/>"
+
     # Start audio file for practice
-    practice_start = "<audio src='https://s3.amazonaws.com/language-of-love/lecturer-practice-start.mp3'/>"
-    
-    # Questions in english represendted by the e. For practice
-    Q_e_lecturer_like_chocolate = "<audio src='https://s3.amazonaws.com/language-of-love/english-lecturer-do-u-like-chocolate.mp3'/>"
-    Q_e_lecturer_like_coffee = "<audio src='https://s3.amazonaws.com/language-of-love/english-lecturer-do-u-like-coffee.mp3'/>"
-    Q_e_lecturer_like_flower = "<audio src='https://s3.amazonaws.com/language-of-love/english-lecturer-do-u-like-flowers.mp3'/>"
-    Q_e_lecturer_like_drink = "<audio src='https://s3.amazonaws.com/language-of-love/english-lecturer-do-u-like-to-drink.mp3'/>"
-    Q_e_lecturer_like_travel = "<audio src='https://s3.amazonaws.com/language-of-love/english-lecturer-do-u-like-to-travel.mp3'/>"
-    Q_e_lecturer_what_name = "<audio src='https://s3.amazonaws.com/language-of-love/english-lecturer-whats-ur-name.mp3'/>"
-    Q_e_lecturer_what_profession = "<audio src='https://s3.amazonaws.com/language-of-love/english-lecturer-whats-ur-profession.mp3'/>"
-   
-    # Questions in spanish represendted by the s. For practice
-    Q_s_lecturer_like_chocolate = "<audio src='https://s3.amazonaws.com/language-of-love/spanish-lecturer-do-u-like-chocolate.mp3'/>"
-    Q_s_lecturer_like_coffee = "<audio src='https://s3.amazonaws.com/language-of-love/spanish-lecturer-do-u-like-coffee.mp3'/>"
-    Q_s_lecturer_like_flower = "<audio src='https://s3.amazonaws.com/language-of-love/spanish-lecturer-do-u-like-flowers.mp3'/>"
-    Q_s_lecturer_like_drink = "<audio src='https://s3.amazonaws.com/language-of-love/spanish-lecturer-do-u-like-to-drink.mp3'/>"
-    Q_s_lecturer_like_travel = "<audio src='https://s3.amazonaws.com/language-of-love/spanish-lecturer-do-u-like-to-travel.mp3'/>"
-    Q_s_lecturer_what_name = "<audio src='https://s3.amazonaws.com/language-of-love/spanish-lecturer-whats-ur-name.mp3'/>"
-    Q_s_lecturer_what_profession = "<audio src='https://s3.amazonaws.com/language-of-love/spanish-lecturer-whats-ur-profession.mp3'/>"
-   
+    practice_start = get_audio("lecturer-practice-start")
+
+    # Questions in english represented by the e. For practice
+    Q_e_lecturer_like_chocolate = get_audio("english-lecturer-do-u-like-chocolate")
+    Q_e_lecturer_like_coffee = get_audio("english-lecturer-do-u-like-coffee")
+    Q_e_lecturer_like_flower = get_audio("english-lecturer-do-u-like-flowers")
+    Q_e_lecturer_like_drink = get_audio("english-lecturer-do-u-like-to-drink")
+    Q_e_lecturer_like_travel = get_audio("english-lecturer-do-u-like-to-travel")
+    Q_e_lecturer_what_name = get_audio("english-lecturer-whats-ur-name")
+    Q_e_lecturer_what_profession = get_audio("english-lecturer-whats-ur-profession")
+
+    # Questions in spanish represented by the s. For practice
+    Q_s_lecturer_like_chocolate = get_audio("spanish-lecturer-do-u-like-chocolate")
+    Q_s_lecturer_like_coffee = get_audio("spanish-lecturer-do-u-like-coffee")
+    Q_s_lecturer_like_flower = get_audio("spanish-lecturer-do-u-like-flowers")
+    Q_s_lecturer_like_drink = get_audio("spanish-lecturer-do-u-like-to-drink")
+    Q_s_lecturer_like_travel = get_audio("spanish-lecturer-do-u-like-to-travel")
+    Q_s_lecturer_what_name = get_audio("spanish-lecturer-whats-ur-name")
+    Q_s_lecturer_what_profession = get_audio("spanish-lecturer-whats-ur-profession")
+
     # In-game audio
-    point = "<audio src='https://s3.amazonaws.com/language-of-love/point.mp3'/>"
+    point = get_audio("point")
 
     # TODO Shorten the cricket sound
-    cricket_sound = "<audio src='https://s3.amazonaws.com/language-of-love/cricket_sound.mp3'/>"
-    
+    cricket_sound = get_audio("cricket_sound")
+
     # Intro files
-    welcome = "<audio src ='https://s3.amazonaws.com/language-of-love/tristan-welcome.mp3'/>"
-    welcome_2 = "<audio src = 'https://s3.amazonaws.com/language-of-love/tristan-welcome-2.mp3'/>"
-    fantastico = "<audio src='https://s3.amazonaws.com/language-of-love/tristan-fantastico.mp3'/>"
-    mucho_gusto = "<audio src='https://s3.amazonaws.com/language-of-love/tristan-mucho_gusto.mp3'/>"
-    pracrice_or_date = "<audio src='https://s3.amazonaws.com/language-of-love/practice_or_speed_date.mp3'/>"
-    
+    welcome = get_audio("tristan-welcome")
+    welcome_2 = get_audio("tristan-welcome-2")
+    fantastico = get_audio("tristan-fantastico")
+    mucho_gusto = get_audio("tristan-mucho_gusto")
+    pracrice_or_date = get_audio("practice_or_speed_date")
+
     # Tessa audio files
-    Q_tessa_how_are_you = "<audio src='https://s3.amazonaws.com/language-of-love/tessa_how_are_you.mp3'/>"
-    Q_tessa_how_old_are_you = "<audio src='https://s3.amazonaws.com/language-of-love/tessa_how_old_are_you.mp3'/>"
-    Q_tessa_how_was_your_day = "<audio src='https://s3.amazonaws.com/language-of-love/tessa_how_was_your_day.mp3'/>"
-    Q_tessa_i_am_from_Wellington = "<audio src='https://s3.amazonaws.com/language-of-love/tessa_i_am_from_Wellington.mp3'/>"
-    Q_tessa_What_is_your_job = "<audio src='https://s3.amazonaws.com/language-of-love/tessa_What_is_your_job.mp3'/>"
-    Q_tessa_what_time_is_it = "<audio src='https://s3.amazonaws.com/language-of-love/tessa_what_time_is_it.mp3'/>"
-    Q_tessa_whats_your_favourite_colour = "<audio src='https://s3.amazonaws.com/language-of-love/tessa_whats_your_favourite_colour.mp3'/>"
-    Q_tessa_whats_your_name = "<audio src='https://s3.amazonaws.com/language-of-love/tessa_whats_your_name.mp3'/>"
-    Q_tessa_where_are_you_from = "<audio src='https://s3.amazonaws.com/language-of-love/tessa_where_are_you_from.mp3'/>"
-    Q_tessa_I_am_lawyer = "<audio src='https://s3.amazonaws.com/language-of-love/tessa_I_am_lawyer.mp3'/>"
-    Q_tessa_i_am_very_tired = "<audio src='https://s3.amazonaws.com/language-of-love/tessa_i_am_very_tired.mp3'/>"
-    A_tessa_im_25_years_old = "<audio src='https://s3.amazonaws.com/language-of-love/tessa_im_25_years_old.mp3'/>"
-    A_tessa_it_is_2_pmn = "<audio src='https://s3.amazonaws.com/language-of-love/tessa_i_am_from_Wtessa_it_is_2_pmellington.mp3'/>"
-    A_tessa_my_favourite_colour_is_blue = "<audio src='https://s3.amazonaws.com/language-of-love/tessa_my_favourite_colour_is_blue.mp3'/>"
-    A_tessa_My_name_is_Tess = "<audio src='https://s3.amazonaws.com/language-of-love/tessa_My_name_is_Tess.mp3'/>"
-    A_tessa_very_good_thanks = "<audio src='https://s3.amazonaws.com/language-of-love/tessa_very_good_thanks.mp3'/>"
+    Q_tessa_how_are_you = get_audio("tessa_how_are_you")
+    Q_tessa_how_old_are_you = get_audio("tessa_how_old_are_you")
+    Q_tessa_how_was_your_day = get_audio("tessa_how_was_your_day")
+    Q_tessa_i_am_from_Wellington = get_audio("tessa_i_am_from_Wellington")
+    Q_tessa_What_is_your_job = get_audio("tessa_What_is_your_job")
+    Q_tessa_what_time_is_it = get_audio("tessa_what_time_is_it")
+    Q_tessa_whats_your_favourite_colour = get_audio("tessa_whats_your_favourite_colour")
+    Q_tessa_whats_your_name = get_audio("tessa_whats_your_name")
+    Q_tessa_where_are_you_from = get_audio("tessa_where_are_you_from")
+    Q_tessa_I_am_lawyer = get_audio("tessa_I_am_lawyer")
+    Q_tessa_i_am_very_tired = get_audio("tessa_i_am_very_tired")
+    A_tessa_im_25_years_old = get_audio("tessa_im_25_years_old")
+    A_tessa_it_is_2_pmn = get_audio("tessa_i_am_from_Wtessa_it_is_2_pmellington")
+    A_tessa_my_favourite_colour_is_blue = get_audio("tessa_my_favourite_colour_is_blue")
+    A_tessa_My_name_is_Tess = get_audio("tessa_My_name_is_Tess")
+    A_tessa_very_good_thanks = get_audio("tessa_very_good_thanks")
 
-    #Carmen Error Messages
-    Carmen_error_message_1 = "<audio src='https://s3.amazonaws.com/language-of-love/Carmen_error_messages/Carmen_Error_1.mp3'/>"
-    Carmen_error_message_2 = "<audio src='https://s3.amazonaws.com/language-of-love/Carmen_error_messages/Carmen_Error_2.mp3'/>"
-    Carmen_error_message_3 = "<audio src='https://s3.amazonaws.com/language-of-love/Carmen_error_messages/Carmen_Error_3.mp3'/>"
+    # Carmen Error Messages
+    Carmen_error_message_1 = get_audio("Carmen_error_messages/Carmen_Error_1")
+    Carmen_error_message_2 = get_audio("Carmen_error_messages/Carmen_Error_2")
+    Carmen_error_message_3 = get_audio("Carmen_error_messages/Carmen_Error_3")
 
-    #Carmen Host
-    Carmen_intro_1 = "<audio src='https://s3.amazonaws.com/language-of-love/Carmen_host/Carmen_intro_1.mp3'/>"
-    Carmen_intro_2 = "<audio src='https://s3.amazonaws.com/language-of-love/Carmen_host/Carmen_intro_2.mp3'/>"
+    # Carmen Host
+    Carmen_intro_1 = get_audio("Carmen_host/Carmen_intro_1")
+    Carmen_intro_2 = get_audio("Carmen_host/Carmen_intro_2")
 
     #
