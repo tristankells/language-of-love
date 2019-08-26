5e17522f3f918082514d600b8bd4d0e305d564ae
Tristan Kells <tristankells@gmail.com>
2019-08-26 21:40:35 +1200

    Trying to refactor the date class a bit to get it working

--- language_of_love/areas/date/date_handler.py
+++ language_of_love/areas/date/date_handler.py
@@ -8,7 +8,7 @@ import json
 import logging
 
 from ask_sdk_core.skill_builder import SkillBuilder
-from ask_sdk_core.utils import is_intent_name
+from ask_sdk_core.utils import is_intent_name, get_intent_name
 from ask_sdk_core.handler_input import HandlerInput
 from areas.date.date_intents.date_helper import date_picker
 
@@ -29,31 +29,34 @@ def can_handle_date(handler_input):
 
     intent_list, response_dict = json.loads(intent_list), json.loads(response_dict)
 
+    intent_name = get_intent_name(handler_input)
+
     if int(session_attr.conversation) == 1000:
-        session_attr = get_variables_not_in_conversation(handler_input, session_attr, intent_list)
+        session_attr = get_variables_not_in_conversation(intent_name, session_attr, intent_list)
 
     elif int(session_attr.conversation) != 1000:
-        session_attr = get_variables_in_conversation(handler_input, session_attr, intent_list)
+        session_attr = get_variables_in_conversation(intent_name, session_attr, intent_list)
 
     z = int(session_attr.conversation)
     y = int(session_attr.place)
 
     handler_input.attributes_manager.session_attributes = session_attr.get_dict()
 
-    return is_intent_name(intent_list[z][y])(handler_input)
+    return intent_list[z][y] is intent_name
 
 
-def get_variables_not_in_conversation(handler_input, session_attr, intent_list):
+def get_variables_not_in_conversation(intent_name, session_attr, intent_list):
+    # If the intent is in the list left site of the intent list, then make conversation = the index of that intent
+    # Make place = 1 indicating a conversation has started, and an answer is expected
     for x in range(0, len(intent_list)):
-        if is_intent_name(intent_list[x][0])(handler_input):
+        if intent_list[x][0] is intent_name:
             session_attr.conversation = x
-    session_attr.place = 0
-    session_attr.conversation = x  # set conversation
+            session_attr.place = 0
     return session_attr
 
 
-def get_variables_in_conversation(handler_input, session_attr, intent_list):
+def get_variables_in_conversation(intent_name, session_attr, intent_list):
     z = session_attr.conversation
-    if is_intent_name(intent_list[z][1])(handler_input):
+    if intent_list[z][1] is intent_name:
         session_attr.place = 1
     return session_attr
