63972013f5c61235d18c0644334b773c60566b74
Tristan Kells <tristankells@gmail.com>
2019-08-26 19:56:15 +1200

    Updated broken reference

--- language_of_love/areas/date/date_intents/date_tessa.py
+++ language_of_love/areas/date/date_intents/date_tessa.py
@@ -6,7 +6,7 @@ from custom_collections.intents import Intents
 
 class Tessa():
     intent_list = [[Intents.QUESTION_WHERE_YOU_FROM, Intents.ANSWER_WHERE_YOU_FROM],
-                   [Intents.QUESTION_JOB, Intents.ANSWER_MY_JOB],
+                   [Intents.QUESTION_JOB, Intents.ANSWER_JOB],
                    [Intents.QUESTION_ANIMALS, "AnswerAnimalIntent"],
                    [Intents.QUESTION_COLOUR, Intents.ANSWER_COLOUR]
                    ]
@@ -15,7 +15,7 @@ class Tessa():
         Intents.QUESTION_WHERE_YOU_FROM: Audio.Q_tessa_i_am_from_Wellington + Audio.Q_tessa_where_are_you_from,
         Intents.ANSWER_WHERE_YOU_FROM: "I love that place",
         Intents.QUESTION_JOB: Audio.Q_tessa_I_am_lawyer + Audio.Q_tessa_What_is_your_job,
-        Intents.ANSWER_MY_JOB: "Great",
+        Intents.ANSWER_JOB: "Great",
         Intents.QUESTION_ANIMALS: "My favourite animal is a giraffe, what's yours?",
         "AnswerAnimalIntent": "Cool animal",
         Intents.QUESTION_COLOUR: Audio.A_tessa_my_favourite_colour_is_blue + Audio.Q_tessa_whats_your_favourite_colour,
